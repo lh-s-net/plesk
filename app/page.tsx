5b5120ff8eefaea762d2f551ca6b0ec1d053e23f
JW <info@lh-s.net>
2023-10-24 13:04:00 +0200

    optimize import

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
-import Image from 'next/image'
-
 export default function Home() {
-  return (
-    <div className="flex justify-center items-center h-screen">
-      <p className="text-4xl">HellowPleskWorld!</p>
-    </div>
-  )
+    return (
+        <div className="flex justify-center items-center h-screen">
+            <p className="text-4xl">HelloPleskWorld!</p>
+        </div>
+    )
 }
